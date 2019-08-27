7edf542f665fdf8ca783af2d959fa43c70c3646c
Jed <alamaresmichaeljed@gmail.com>
2019-08-27 19:26:11 +0800

    done on cart increment

--- src/components/collection-item/collection-item.jsx
+++ src/components/collection-item/collection-item.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import './collection-item.scss'
+import CustomButton from '../../components/custom-button/custom-button'
+import { addItem } from '../../redux/cart/cart-actions' 
 
-const CollectionItem = ({ name, price, imageUrl }) => (
-  <div className="collection-item">
-    <img src={imageUrl} alt={name} className="image"/>
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item
+
+
+  return (
+    <div className="collection-item">
+      <img src={imageUrl} alt={name} className="image"/>
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <CustomButton onClick={() => addItem(item)} inverted>ADD TO CART</CustomButton>
     </div>
-  </div>
-)
+  )
+}
 
-export default CollectionItem
+const mapDispatchToProps = dispatch => {
+  return {
+    addItem: item => dispatch(addItem(item))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(CollectionItem)
